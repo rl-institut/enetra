15084ef6e325a153eae0e530fa967f8aaba195e5
PaulScheer <paul.scheer@rl-institut.de>
2025-07-22 14:25:00 +0200

    Fix packages so oemof can run. Add dummy view for simulation

--- enetra/urls.py
+++ enetra/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
@@ -22,4 +24,8 @@ from django.urls import path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
+    path("ports/", include("ports.urls")),
+    # Media files
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+    path("oemof/", include("django_oemof.urls")),
 ]
