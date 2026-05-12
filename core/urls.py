7c035863e06f0e11fca783e090d513a9e1a1bd5e
PaulScheer <paul.scheer@rl-institut.de>
2026-05-12 13:32:19 +0200

    Add enetra_components pages

--- core/urls.py
+++ core/urls.py
@@ -16,4 +16,54 @@ urlpatterns = [
         lambda x: render(x, template_name="cotton/tabs/test.html"),
         name="tabs_test",
     ),
+    path(
+        "landing/",
+        lambda x: render(x, template_name="core/landing_page.html"),
+        name="landing_page",
+    ),
+    path(
+        "login/",
+        lambda x: render(x, template_name="core/login.html"),
+        name="login",
+    ),
+    path(
+        "register/",
+        lambda x: render(x, template_name="core/register.html"),
+        name="register",
+    ),
+    path(
+        "reset_password/",
+        lambda x: render(x, template_name="core/reset-password.html"),
+        name="reset_password",
+    ),
+    path(
+        "forgot_password/",
+        lambda x: render(x, template_name="core/forgot-password.html"),
+        name="forgot_password",
+    ),
+    path(
+        "projects/",
+        lambda x: render(x, template_name="core/projects.html"),
+        name="projects",
+    ),
+    path(
+        "user_rechte/",
+        lambda x: render(x, template_name="core/user-rechte.html"),
+        name="user_rechte",
+    ),
+    path(
+        "ergebnisse/",
+        lambda x: render(x, template_name="core/ergebnisse.html"),
+        name="ergebnisse",
+    ),
+    path(
+        "einstellungen/",
+        lambda x: render(x, template_name="core/einstellungen.html"),
+        name="einstellungen",
+    ),
+    path(
+        "szenarienvergleich/",
+        lambda x: render(x, template_name="core/szenarienvergleich.html"),
+        name="szenarienvergleich",
+    ),
 ]
