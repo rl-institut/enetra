127a2b5479c8eb598dcd62093b8c41766e06f848
PaulScheer <paul.scheer@rl-institut.de>
2026-05-18 16:56:22 +0200

    Add basic signup, login and logout

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,52 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(label=_("Vorname (optional)"), max_length=64, required=False)
+    last_name = forms.CharField(label=_("Nachname (optional)"), max_length=64, required=False)
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={"autocomplete": "username", "autofocus": True}),
+        label=_("E-Mail*"),
+        max_length=254,
+        required=True,
+    )
+    password1 = forms.CharField(label=_("Passwort*"), required=True, widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label=_("Passwort wiederholen*"), required=True, widget=forms.PasswordInput
+    )
+    accepts_terms = forms.BooleanField(required=True)
+
+    class Meta:
+        model = User
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+            "accepts_terms",
+        )
+
+    def clean_accepts_terms(self):
+        accepts_terms = self.cleaned_data["accepts_terms"]
+        if not accepts_terms:
+            raise forms.ValidationError(
+                _("Für einen Zugang müssen Sie der Datenschutzerklärung zustimmen.")
+            )
+        return True
+
+    def clean_email(self):
+        """
+        Check that lowercase user email is unique (used as username)
+        """
+        email = self.cleaned_data["email"]
+        if User.objects.filter(username=email.lower()).exists():
+            raise forms.ValidationError(email.lower() + _(" existiert bereits."))
+        return email
 
 
 class AuthForm(AuthenticationForm):
