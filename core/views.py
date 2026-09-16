7e52977fdb22569cd2370df5dbd31dc39d8ffd8a
stefansc1 <stefan.schirmeister@localiser.de>
2026-09-16 14:43:59 +0200

    fix ports imports

--- core/views.py
+++ core/views.py
@@ -38,19 +38,19 @@ from django.views.generic import TemplateView
 from core.models import Invite
 from core.models import InviteError
 from core.models import Role
-from ports.authorization import has_authorization
-from ports.forms import CreateProjectForm
-from ports.models import Project
-from ports.models import Scenario
-from ports.models import ScenarioItem
-from ports.util import get_template_scenarios
-from ports.util import get_user_projects
-from ports.util import prefetch_projects_users
 
+from .authorization import has_authorization
 from .forms import AuthForm
 from .forms import ChangeAccountDataForm
+from .forms import CreateProjectForm
 from .forms import InviteForm
 from .forms import SignUpForm
+from .models import Project
+from .models import Scenario
+from .models import ScenarioItem
+from .util import get_template_scenarios
+from .util import get_user_projects
+from .util import prefetch_projects_users
 
 
 class LoginViewWithRemember(LoginView):
